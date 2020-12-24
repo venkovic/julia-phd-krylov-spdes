064045775051dc9e823c97a160cbf4d6c82bdec3
nvenkov1 <venkovic@gmail.com>
2020-12-24 13:43:03 +0100

    Changed in set_subdomains()

--- Example03_dd.jl
+++ Example03_dd.jl
@@ -6,9 +6,9 @@ using Fem
 poly = polygon_unitSquare()
 mesh = create_mesh(poly, info_str="my mesh", voronoi=true, delaunay=true, set_area_max=true)
 
-nd = 400
-epart, npart = mesh_partition(mesh, nd)
-nodes_at_interface = set_subdomains(mesh, epart)
+ndom = 400
+epart, npart = mesh_partition(mesh, ndom)
+elemd, node_Γ, node_Id, ind_Id, nn_Id, is_on_Γ = set_subdomains(mesh, epart, npart)
 
 function a(x::Float64, y::Float64)
   return .1 + .0001 * x * y
@@ -22,8 +22,15 @@ function uexact(xx, yy)
   return .734
 end
 
+#A_IId = do_IId_assembly(elemd, cell, point, is_on_Γ, ind_Id, nn_Id, a, f, 1)
+#do_IΓ_assembly()
+#do_ΓΓ_assembly()
+
 @time npzwrite("cells.npz", mesh.cell' .- 1)
 @time npzwrite("points.npz", mesh.point')
 
 @time npzwrite("epart.npz", epart .- 1)
-@time npzwrite("nodes_at_interface.npz", nodes_at_interface .- 1)
+@time npzwrite("nodes_at_interface.npz", node_Γ .- 1)
+for id in 1:ndom
+  npzwrite("nodes_inside_$id.npz", node_Id[id] .- 1)
+end
--- Example03_dd.py
+++ Example03_dd.py
@@ -7,8 +7,14 @@ epart = np.load('epart.npz')[:, 0]
 points = np.load('points.npz')
 #u = np.load('u.npz')
 
+nd = epart.max() + 1
+
 fig, axes = pl.subplots()
 axes = to_axis(epart, cells, points, axes, at='elems')
 nodes_at_interface = np.load('nodes_at_interface.npz')
+nodes_inside = [np.load('nodes_inside_%d.npz' % (i+1)) for i in range(nd)]
+
 pl.scatter(points[nodes_at_interface, 0], points[nodes_at_interface, 1], s=1, c='k')
+for id in range(nd):
+  pl.scatter(points[nodes_inside[id], 0], points[nodes_inside[id], 1], s=1, c=np.random.rand(3))
 pl.show()
