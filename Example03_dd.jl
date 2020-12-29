4ba831b3b414899208467be49bf3c4a0e77024d9
nvenkov1 <venkovic@gmail.com>
2020-12-29 17:15:10 +0100

    Started applying Schur operator in example.

--- Example03_dd.jl
+++ Example03_dd.jl
@@ -2,13 +2,16 @@ using TriangleMesh
 using NPZ
 push!(LOAD_PATH, "./Fem/")
 using Fem
+using LinearMaps
+using IterativeSolvers
 
 poly = polygon_unitSquare()
 mesh = create_mesh(poly, info_str="my mesh", voronoi=true, delaunay=true, set_area_max=true)
 
 ndom = 400
 epart, npart = mesh_partition(mesh, ndom)
-elemd, node_Γ, node_Id, ind_Id, nn_Id, is_on_Γ = set_subdomains(mesh, epart, npart)
+ind_Id, ind_Γ, is_on_Γ, elemd, node_Γ, node_Id, nn_Id = set_subdomains(mesh, epart, npart)
+# only ind_Id, ind_Γ, is_on_Γ are essential
 
 function a(x::Float64, y::Float64)
   return .1 + .0001 * x * y
@@ -22,15 +25,30 @@ function uexact(xx, yy)
   return .734
 end
 
-#A_IId = do_IId_assembly(elemd, cell, point, is_on_Γ, ind_Id, nn_Id, a, f, 1)
-#do_IΓ_assembly()
-#do_ΓΓ_assembly()
+A_IId, A_IΓd, A_ΓΓ, b_I, b_Γ = do_schur_assembly(mesh.cell, mesh.point, epart, ind_Id, ind_Γ, is_on_Γ, a, f)
+n_Γ, _ = size(A_ΓΓ)
+S = LinearMap(x -> apply_schur(A_IId, A_IΓd, A_ΓΓ, x), n_Γ, issymmetric=true)
+
+
+# To do: - apply BC
+#        - get ̂b, i.e., RHS and Schur RHS
+#
+u_Γ = rand(n_Γ)
+cg!(S, u_Γ, ̂b)
+# Compute u_Id
+# Compare with regular solve
+
+#using Distributed
+#addprocs(2)
+#addprocs(([("marcel", 4)]), tunnel=true)
+#addprocs(([("andrew", 4)]), tunnel=true)
 
 @time npzwrite("cells.npz", mesh.cell' .- 1)
 @time npzwrite("points.npz", mesh.point')
 
-@time npzwrite("epart.npz", epart .- 1)
-@time npzwrite("nodes_at_interface.npz", node_Γ .- 1)
-for id in 1:ndom
-  npzwrite("nodes_inside_$id.npz", node_Id[id] .- 1)
-end
+#@time npzwrite("epart.npz", epart .- 1)
+#@time npzwrite("nodes_at_interface.npz", node_Γ .- 1)
+#for id in 1:ndom
+#  npzwrite("nodes_inside_$id.npz", node_Id[id] .- 1)
+#end
+
