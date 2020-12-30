7364718df6b565d9e13fd2a44cf70db5986d67d1
nvenkov1 <venkovic@gmail.com>
2020-12-30 15:13:37 +0100

    Started solving local KL problems.

--- Example04_KarhunenLoevePllDomainDecomposition.jl
+++ Example04_KarhunenLoevePllDomainDecomposition.jl
@@ -1,7 +1,31 @@
-using Distributed
+#using Distributed
+#addprocs(3)
+#addprocs(([("marcel", 4)]), tunnel=true)
+#addprocs(([("andrew", 4)]), tunnel=true)
 
-addprocs(2)
-addprocs(([("marcel", 1)]), tunnel=true)
-addprocs(([("andrew", 1)]), tunnel=true)
+push!(LOAD_PATH, "./Fem/")
+using Fem
+using TriangleMesh
+using NPZ
+using Arpack
 
-println(nworkers())
+poly = polygon_unitSquare()
+mesh = create_mesh(poly, info_str="my mesh", voronoi=true, delaunay=true, set_area_max=true)
+
+ndom = 40
+epart, npart = mesh_partition(mesh, ndom)
+
+L = .1
+sig2 = 1.
+
+function cov(x1::Float64, y1::Float64, x2::Float64, y2::Float64)
+  return sig2 * exp(-((x1 - x2)^ 2 + (y1 - y2)^2) / L^2)
+end
+
+for idom in 1:ndom
+  inds_l2g, inds_g2l, elems = set_subdomain(mesh, epart, idom)
+  C = do_local_mass_covariance_assembly(mesh.cell, mesh.point, inds_l2g, inds_g2l, elems, cov)
+  M = do_local_mass_assembly(mesh.cell, mesh.point, inds_g2l, elems)
+  λ, Φ = map(x -> real(x), eigs(C, M))
+  println("$idom, $(length(inds_l2g)), $(sum(λ))")
+end
