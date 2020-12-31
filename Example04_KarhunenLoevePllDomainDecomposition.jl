aa1986ac8ee269ba0a25d482eecca45ae27f70f9
nvenkov1 <venkovic@gmail.com>
2020-12-31 16:38:12 +0100

    Added drawing of realizations by serial DD KL.

--- Example04_KarhunenLoevePllDomainDecomposition.jl
+++ Example04_KarhunenLoevePllDomainDecomposition.jl
@@ -7,12 +7,15 @@ push!(LOAD_PATH, "./Fem/")
 using Fem
 using TriangleMesh
 using NPZ
-using Arpack
+import LinearAlgebra
+import Arpack
+using Distributions
 
 poly = polygon_unitSquare()
 mesh = create_mesh(poly, info_str="my mesh", voronoi=true, delaunay=true, set_area_max=true)
 
-ndom = 40
+ndom = 20
+nev = 35
 epart, npart = mesh_partition(mesh, ndom)
 
 L = .1
@@ -22,10 +25,30 @@ function cov(x1::Float64, y1::Float64, x2::Float64, y2::Float64)
   return sig2 * exp(-((x1 - x2)^ 2 + (y1 - y2)^2) / L^2)
 end
 
+inds_g2ld = [Dict{Int,Int}() for _ in 1:ndom]
+inds_l2gd = Array{Int,1}[]
+elemsd = Array{Int,1}[]
+ϕd = Array{Float64,2}[]
+
 for idom in 1:ndom
   inds_l2g, inds_g2l, elems = set_subdomain(mesh, epart, idom)
   C = do_local_mass_covariance_assembly(mesh.cell, mesh.point, inds_l2g, inds_g2l, elems, cov)
   M = do_local_mass_assembly(mesh.cell, mesh.point, inds_g2l, elems)
-  λ, Φ = map(x -> real(x), eigs(C, M))
+  λ, ϕ = map(x -> real(x), Arpack.eigs(C, M, nev=nev))
+  inds_g2ld[idom] = inds_g2l
+  push!(inds_l2gd, inds_l2g)
+  push!(elemsd, elems)
+  push!(ϕd, ϕ)
   println("$idom, $(length(inds_l2g)), $(sum(λ))")
 end
+
+#M = do_global_mass_reduced_assembly(mesh.cell, mesh.point, epart, inds_g2ld, ϕd)
+K = @time do_global_mass_covariance_reduced_assembly(mesh.cell, mesh.point, elemsd, inds_g2ld, inds_l2gd, ϕd, cov)
+
+Λ, Φ = @time LinearAlgebra.eigen(K)
+
+ξ, g = @time draw(mesh, Λ, Φ, ϕd, inds_l2gd)
+
+@time npzwrite("cells.npz", mesh.cell' .- 1)
+@time npzwrite("points.npz", mesh.point')
+@time npzwrite("g.npz", g)
