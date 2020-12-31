24e6b65dfd4fed1e20147b5ca1e4bcd52f23821e
nvenkov1 <venkovic@gmail.com>
2020-12-31 16:39:05 +0100

    Minor changes.

--- Example02_KarhunenLoeve.jl
+++ Example02_KarhunenLoeve.jl
@@ -2,21 +2,17 @@ using TriangleMesh
 push!(LOAD_PATH, "./Fem/")
 using Fem
 using Arpack
+using Distributions
 using NPZ
 
 poly = polygon_Lshape()
 mesh = create_mesh(poly, info_str="my mesh", voronoi=true, delaunay=true, set_area_max=true)
-#fig = plot_TriMesh(mesh)
 
-nnode = mesh.n_point
-p = mesh.point
-
-nel = mesh.n_cell
-nodes = mesh.cell
+L = .1
+sig2 = 1.
 
 function cov(x1::Float64, y1::Float64, x2::Float64, y2::Float64)
-  L = .1
-  return exp(-((x1 - x2)^ 2 + (y1 - y2)^2) / L^2)
+  return sig2 * exp(-((x1 - x2)^ 2 + (y1 - y2)^2) / L^2)
 end
   
 C = @time do_mass_covariance_assembly(mesh.cell, mesh.point, cov)
@@ -24,4 +20,20 @@ M = @time get_mass_matrix(mesh.cell, mesh.point)
 
 λ, Φ = map(x -> real(x), eigs(C, M, nev=400))
 
+function draw(λ, Φ)
+  n, nmode = size(Φ)
+  ξ = rand(Normal(), nmode)
+  g = zeros(n)
+  for k in 1:nmode
+    g .+= sqrt(λ[k]) * ξ[k] * Φ[:, k]
+  end
+  return ξ, g
+end
+
 area = get_total_area(mesh.cell, mesh.point)
+
+ξ, g = draw(λ, Φ)
+
+@time npzwrite("cells.npz", mesh.cell' .- 1)
+@time npzwrite("points.npz", mesh.point')
+@time npzwrite("g.npz", g)
