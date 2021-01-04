a9b8e2c9a339bb06432823b2897f5e60d32338b5
nvenkov1 <venkovic@gmail.com>
2021-01-05 00:15:09 +0100

    Added call to solve_global_reduced_kl() in Example05.

--- Example05_KarhunenLoevePllDomainDecomposition.jl
+++ Example05_KarhunenLoevePllDomainDecomposition.jl
@@ -1,7 +1,9 @@
 using Distributed
-addprocs(2)
+
 addprocs(([("marcel", 4)]), tunnel=true)
 addprocs(([("andrew", 4)]), tunnel=true)
+#addprocs(([("moorcock", 4)]), tunnel=true)
+addprocs(3)
 
 @everywhere begin
   push!(LOAD_PATH, "./Fem/")
@@ -11,52 +13,72 @@ end
 
 @everywhere begin 
   using Fem
-  using TriangleMesh
-  import LinearAlgebra
-  import Arpack
   using Distributed
   using Distributions
-  using Distributed
   using DistributedOperations
 end
 
+import LinearAlgebra
 using NPZ
 
+# Advice:
+# - At fixed value of ndom * nev, 
+#   use large ndom and small nev.
+
 @everywhere begin
-  ndom = 20
-  nev = 35
+  ndom = 300
+  nev = 10
+  tentative_nnode = 200_000
+  forget = 1e-6
 end
 
-poly = polygon_unitSquare()
-mesh = create_mesh(poly, info_str="my mesh", voronoi=true,
-                   delaunay=true, set_area_max=true)
+mesh = get_mesh(tentative_nnode)
+npzwrite("cells.npz", mesh.cell' .- 1)
+npzwrite("points.npz", mesh.point')
 epart, npart = mesh_partition(mesh, ndom)
-
 bcast(mesh, procs())
 bcast(epart, procs())
 
-@everywhere begin
+@everywhere function cov(x1::Float64, y1::Float64, x2::Float64, y2::Float64)
   L = .1
   sig2 = 1.
-  function cov(x1::Float64, y1::Float64, x2::Float64, y2::Float64)
-    return sig2 * exp(-((x1 - x2)^ 2 + (y1 - y2)^2) / L^2)
+  return sig2 * exp(-((x1 - x2)^ 2 + (y1 - y2)^2) / L^2)
+end
+
+# Distributed computation of local KL expansion for each subdomain  
+@time domain = @sync @distributed merge! for idom in 1:ndom
+  pll_solve_local_kl(mesh, epart, cov, nev, idom, 
+                     forget=forget, relative=.996)
+end
+
+energy_expected = 0.
+for idom in 1:ndom
+  global energy_expected += domain[idom].energy
+end
+
+# Count number of local modes retained in each subdomain
+md = zeros(Int, ndom) 
+for idom in 1:ndom
+  md[idom] = size(domain[idom].ϕ)[2]
+end
+bcast(md, procs())
+
+# Do distributed assembly of globally reduced system
+@time begin
+  K = @sync @distributed (+) for idom in 1:ndom
+  pll_do_global_mass_covariance_reduced_assembly(mesh.cell, mesh.point, 
+                                                 domain, idom, md, cov,
+                                                 forget=forget, 
+                                                 relative=.996)
   end
 end
 
-#@everywhere inds_g2ld = [Dict{Int,Int}() for _ in 1:ndom]
-#@everywhere inds_l2gd = Array{Int,1}[]
-#@everywhwre elemsd = Array{Int,1}[]
-#@everywhere ϕd = Array{Float64,2}[]
+# Solve globally reduced eigenvalue problem, and project eigenfunctions 
+Λ, Ψ = @time solve_global_reduced_kl(mesh, K, energy_expected,
+                                     domain, relative=.995)
+npzwrite("kl-eigvals.npz", Λ)
+npzwrite("kl-eigvecs.npz", Ψ)
 
-@sync @distributed for idom in 1:ndom
-  inds_l2g, inds_g2l, elems = set_subdomain(mesh, epart, idom)
-  C = do_local_mass_covariance_assembly(mesh.cell, mesh.point, inds_l2g, inds_g2l, 
-                                        elems, cov)
-  M = do_local_mass_assembly(mesh.cell, mesh.point, inds_g2l, elems)
-  λ, ϕ = map(x -> real(x), Arpack.eigs(C, M, nev=nev))
-  #inds_g2ld[idom] = inds_g2l
-  #push!(inds_l2gd, inds_l2g)
-  #push!(elemsd, elems)
-  #push!(ϕd, ϕ)
-  println("$idom, $(length(inds_l2g)), $(sum(λ))")
-end
+# Sample
+ξ, g = @time draw(Λ, Ψ)
+@time npzwrite("greal.npz", g)
