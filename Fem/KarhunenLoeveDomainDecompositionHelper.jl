719c3b2dbc78f48075a5ea028195f88ca7534d75
nvenkov1 <venkovic@gmail.com>
2021-01-05 22:22:21 +0100

    Changed parameter suggestion.

--- Fem/KarhunenLoeveDomainDecompositionHelper.jl
+++ Fem/KarhunenLoeveDomainDecompositionHelper.jl
@@ -38,14 +38,11 @@ ndom = 300
 """
 function suggest_parameters(nnode::Int)
   if nnode >= 1_000_000
-    relative_local = .99996
-    relative_global = .99995
+    return .99996, .99995
   elseif nnode >= 100_000
-    relative_local = .99996 
-    relative_global = .99995
+    return .9996, .9995
   else
-    relative_local = .996
-    relative_global = .995
+    return .996, .995
   end
   return relative_local, relative_global
 end
