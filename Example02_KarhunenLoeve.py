85e8e8b6bbfd0982a2ae049e734f6e14ba667d55
nvenkov1 <venkovic@gmail.com>
2021-01-14 23:32:44 +0100

    Example02_KarhunenLoeve.jl

--- Example02_KarhunenLoeve.py
+++ Example02_KarhunenLoeve.py
@@ -1,11 +1,17 @@
 import numpy as np
 import pylab as pl
-from postproc_utils import to_axis
+from postproc_utils import to_axis, get_root_filename
 
-cells = np.load('cells.npz')
-points = np.load('points.npz')
-g = np.load('g.npz')
+tentative_nnode = 5_000
+model = 'SExp'
+L = .1
+sig2 = 1.
+root_fname = get_root_filename(model, sig2, L, tentative_nnode)
+
+cells = np.load('data/DoF%d.cells.npz' % tentative_nnode)
+points = np.load('data/DoF%d.points.npz' % tentative_nnode)
+g = np.load('data/%s.greal.npz' % root_fname)
 
 fig, axes = pl.subplots()
-axes = to_axis(g, cells, points, axes, plot='normal') # vmin=.3, vmax=.8)
+axes = to_axis(g, cells, points, axes, plot='normal')
 pl.show()
