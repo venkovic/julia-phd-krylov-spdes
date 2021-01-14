166a9cbdea5bf1551dfd36c21bb2a46c4e425741
nvenkov1 <venkovic@gmail.com>
2021-01-14 14:11:28 +0100

    Keep examples up to date.

--- Example08_EllipticPdePllDomainDecomposition.jl
+++ Example08_EllipticPdePllDomainDecomposition.jl
@@ -1,46 +1,82 @@
-#using TriangleMesh
-#using NPZ
-push!(LOAD_PATH, "./Fem/")
+using Distributed
 
-import Pkg
-Pkg.activate(".")
+addprocs(([("marcel", 4)]), tunnel=true)
+addprocs(([("andrew", 3)]), tunnel=true)
+#addprocs(([("moorcock", 4)]), tunnel=true)
+addprocs(3)
 
-using Fem
+@everywhere begin
+  push!(LOAD_PATH, "./Fem/")
+  import Pkg
+  Pkg.activate(".")
+end
+
+@everywhere begin 
+  using Fem
+  using Distributed
+  using Distributions
+  using DistributedOperations
+end
+
+
+
+
+
+
+
+
+import LinearAlgebra
+using NPZ
+
+
+
+
+
+
+
 using LinearMaps
 using IterativeSolvers
 using Preconditioners
 
-tentative_nnode = 100_000
+
+
+
+
+
+
+
+@everywhere begin
+  ndom = 400
+  tentative_nnode = 100_000
+end
+
 mesh = get_mesh(tentative_nnode)
 
 dirichlet_inds_g2l, not_dirichlet_inds_g2l,
 dirichlet_inds_l2g, not_dirichlet_inds_l2g = 
 get_dirichlet_inds(mesh.point, mesh.point_marker)
 
-ndom = 400
 epart, npart = mesh_partition(mesh, ndom)
 ind_Id_g2l, ind_Γ_g2l, node_owner, 
 elemd, node_Γ, node_Id, nnode_Id = set_subdomains(mesh, epart, npart, dirichlet_inds_g2l)
-# only ind_Id_g2l, ind_Γ_g2l and node_owner are essential
 
-function a(x::Float64, y::Float64)
+bcast(mesh, procs())
+bcast(epart, procs())
+bcast(ind_Id_g2l, procs())
+bcast(ind_Γ_g2l, procs())
+bcast(node_owner, procs())
+
+@everywhere function a(x::Float64, y::Float64)
   return .1 + .0001 * x * y
 end
   
-function f(x::Float64, y::Float64)
+@everywhere function f(x::Float64, y::Float64)
   return -1.
 end
 
-function uexact(xx::Float64, yy::Float64)
+@everywhere function uexact(xx::Float64, yy::Float64)
   return .734
 end
-
-print("do_isotropic_elliptic_assembly ...")
-A, b = @time do_isotropic_elliptic_assembly(mesh.cell, mesh.point,
-                                            dirichlet_inds_g2l,
-                                            not_dirichlet_inds_g2l,
-                                            mesh.point_marker,
-                                            a, f, uexact)
 
 print("do_schur_assembly ...")
 A_IId, A_IΓd, A_ΓΓ, b_Id, b_Γ = @time do_schur_assembly(mesh.cell,
@@ -73,16 +109,47 @@ u_with_dd = merge_subdomain_solutions(u_Γ, u_Id, node_Γ, node_Id,
                                       dirichlet_inds_l2g, uexact,
                                       mesh.point)
 
-print("assemble AMG preconditioner ...")
-Π = @time AMGPreconditioner{SmoothedAggregation}(A)
 
-print("solve for u_no_dd_no_dirichlet ...")
-u_no_dd_no_dirichlet = @time IterativeSolvers.cg(A, b, Pl=Π)
 
-u_no_dd = append_bc(dirichlet_inds_l2g, not_dirichlet_inds_l2g,
-                    u_no_dd_no_dirichlet, mesh.point, uexact)
 
-print("extrema(u_with_dd - u_no_dd) = $(extrema(u_with_dd - u_no_dd))")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
