52206a1149a619535f100baf94f36808aeeef959
nvenkov1 <venkovic@gmail.com>
2021-01-14 23:52:22 +0100

    Updated Exampl04.

--- Example04_KarhunenLoeveDomainDecomposition.jl
+++ Example04_KarhunenLoeveDomainDecomposition.jl
@@ -3,22 +3,39 @@ import Pkg
 Pkg.activate(".")
 using Fem
 using NPZ
-import LinearAlgebra
-import Arpack
-using Distributions
 
-# Advice:
-# - At fixed value of ndom * nev, 
-#   use large ndom and small nev.
+tentative_nnode = 20_000
+load_existing_mesh = false
 
 ndom = 80
+load_existing_partition = false
+
 nev = 35
-tentative_nnode = 20_000
 forget = 1e-6
 
-mesh = get_mesh(tentative_nnode)
-save_mesh(tentative_nnode)
-epart, npart = mesh_partition(mesh, ndom)
+if load_existing_mesh
+  cells, points, point_markers, cell_neighbors = load_mesh(tentative_nnode)
+  _, nnode = size(points)
+else
+  mesh = get_mesh(tentative_nnode)
+  save_mesh(mesh, tentative_nnode)
+  cells = mesh.cell
+  points = mesh.point
+  point_markers = mesh.point_marker
+  cell_neighbors = mesh.cell_neighbor
+  _, nnode = size(points)
+end
+
+dirichlet_inds_g2l, not_dirichlet_inds_g2l,
+dirichlet_inds_l2g, not_dirichlet_inds_l2g = 
+get_dirichlet_inds(points, point_markers)
+
+if load_existing_partition
+  epart, npart = load_partition(tentative_nnode, ndom)
+else
+  epart, npart = mesh_partition(cells, ndom)
+  save_partition(epart, npart, tentative_nnode, ndom)
+end
 
 model = "SExp"
 sig2 = 1.
@@ -31,6 +48,8 @@ function cov(x1::Float64, y1::Float64, x2::Float64, y2::Float64)
   return sig2 * exp(-((x1 - x2)^ 2 + (y1 - y2)^2) / L^2)
 end
 
+relative_local, relative_global = suggest_parameters(nnode)
+
 inds_g2ld = [Dict{Int,Int}() for _ in 1:ndom]
 inds_l2gd = Array{Int,1}[]
 elemsd = Array{Int,1}[]
@@ -38,9 +57,9 @@ elemsd = Array{Int,1}[]
 centerd = Array{Float64,1}[]
 energy_expected = 0.
 
-# Loop over subdomains and compute local KL expansions
+println("solve_local_kl ...")
 @time for idom in 1:ndom
-  subdom = solve_local_kl(mesh, epart, cov, nev, idom, relative=.996)
+  subdom = solve_local_kl(cells, points, epart, cov, nev, idom, relative=relative_local)
   inds_g2ld[idom] = subdom.inds_g2l
   push!(inds_l2gd, subdom.inds_l2g)
   push!(elemsd, subdom.elems)
@@ -48,20 +67,24 @@ energy_expected = 0.
   push!(centerd, subdom.center)
   global energy_expected += subdom.energy
 end
+println("... done with solve_local_kl.")
 
-# Assemble globally reduced eigenvalue problem
-K = @time do_global_mass_covariance_reduced_assembly(mesh.cell, mesh.point, elemsd,
+println("do_global_mass_covariance_reduced_assembly ...")
+K = @time do_global_mass_covariance_reduced_assembly(cells, points, elemsd,
                                                      inds_g2ld, inds_l2gd, ϕd,
                                                      centerd, cov, forget=forget)
+println("done with do_global_mass_covariance_reduced_assembly.")
 
-# Solve globally reduced eigenvalue problem, and project eigenfunctions 
-Λ, Ψ = @time solve_global_reduced_kl(mesh, K, energy_expected,
+println("solve_global_reduced_kl ...")
+Λ, Ψ = @time solve_global_reduced_kl(nnode, K, energy_expected,
                                      elemsd, inds_l2gd, ϕd,
-                                     relative=.995)
+                                     relative=relative_global)
 npzwrite("data/$root_fname.kl-eigvals.npz", Λ)
 npzwrite("data/$root_fname.kl-eigvecs.npz", Ψ)
 
-# Sample
+print("sample ...")
 ξ, g = @time draw(Λ, Ψ)
+
+print("sample in place ...")
 @time draw!(Λ, Ψ, ξ, g)
 npzwrite("data/$root_fname.greal.npz", g)
--- Example04_KarhunenLoeveDomainDecomposition.py
+++ Example04_KarhunenLoeveDomainDecomposition.py
@@ -1,16 +1,17 @@
 import numpy as np
 import pylab as pl
-from postproc_utils import to_axis
+from postproc_utils import to_axis, get_root_filename
 
+tentative_nnode = 20_000
+model = 'SExp'
+L = .1
+sig2 = 1.
+root_fname = get_root_filename(model, sig2, L, tentative_nnode)
 
-tentative_node = 400_000
-mesh = "DoF%s" % tentative_node
-model = "SExp_sig21.0_L0.1_DoF%s" % tentative_node
-
-cells = np.load('data/%s.cells.npz' % mesh)
-points = np.load('data/%s.points.npz' % mesh)
-g = np.load('data/%s.greal.npz' % model)
+cells = np.load('data/DoF%d.cells.npz' % tentative_nnode)
+points = np.load('data/DoF%d.points.npz' % tentative_nnode)
+g = np.load('data/%s.greal.npz' % root_fname)
 
 fig, axes = pl.subplots()
-axes = to_axis(g, cells, points, axes, plot='normal') # vmin=.3, vmax=.8)
+axes = to_axis(g, cells, points, axes, plot='normal')
 pl.show()
