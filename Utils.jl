51ec74e3ae2a87603e71f0bfc8b985116dae7b62
nvenkov1 <venkovic@gmail.com>
2021-01-21 20:17:29 +0100

    Moved Utils.jl

--- Utils.jl
+++
@@ -1,9 +0,0 @@
-function printlnln(str::String, width=2)
-  println()
-  println(str)
-  print(" " ^ width)
-end
-  
-function space_println(str::String, width=4)
-  println(" " ^ width * str)
-end  
