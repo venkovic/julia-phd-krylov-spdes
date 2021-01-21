21e8f46894296aeda58a9359b798e36456ddf09d
nvenkov1 <venkovic@gmail.com>
2021-01-21 19:45:27 +0100

    First commit of Utils.jl

---
+++ Utils.jl
@@ -0,0 +1,9 @@
+function printlnln(str::String, width=2)
+  println()
+  println(str)
+  print(" " ^ width)
+end
+  
+function space_println(str::String, width=4)
+  println(" " ^ width * str)
+end  
