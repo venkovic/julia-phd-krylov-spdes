9dc716c4c2e12972a8a6a091f21d52d2353b6904
nvenkov1 <venkovic@gmail.com>
2021-02-03 14:20:16 +0100

    Put some order in the header file, i.e., Fem.jl.

--- Fem/Fem.jl
+++ Fem/Fem.jl
@@ -1,79 +1,100 @@
 module Fem
 
-export SubDomain
+# from Mesh.jl
+export get_mesh,
+       save_mesh,
+       load_mesh
+export get_total_area
+export mesh_partition,
+       save_partition,
+       load_partition
 
-export do_isotropic_elliptic_assembly
-export update_isotropic_elliptic_assembly!
+# from BoundaryConditions.jl
+export get_dirichlet_inds,
+       apply_dirichlet,
+       append_bc
+
+# from EllipticPde.jl
+export do_isotropic_elliptic_assembly,
+       update_isotropic_elliptic_assembly!
 export get_mass_matrix
 
-export get_dirichlet_inds
-export apply_dirichlet
-export append_bc
-
-export do_mass_covariance_assembly
-export solve_kl
-
-export get_mesh
-export save_mesh
-export load_mesh
-export mesh_partition
-export save_partition
-export load_partition
-export get_total_area
-
+# from EllipticPdeDomainDecomposition.jl,
+export SubDomain
 export set_subdomain
 export set_subdomains
 
-export prepare_global_schur
-export prepare_local_schurs
-export apply_global_schur
-export apply_local_schur
-export apply_local_schurs
-export assemble_local_schurs
-export apply_neumann_neumann_schur
-export NeumannNeumannSchurPreconditioner
-export prepare_neumann_neumann_schur_precond
-export apply_neumann_neumann_induced
-export NeumannNeumannInducedPreconditioner
-export prepare_neumann_neumann_induced_precond
-export apply_domain_decomposition_low_rank
-export DomainDecompositionLowRankPreconditioner
-export prepare_domain_decomposition_low_rank_precond
-export apply_inv_a0, apply_inv_a0!, apply_hmat
-export apply_lorasc
-export LorascPreconditioner
-export prepare_lorasc_precond
-export get_schur_rhs
-export get_subdomain_solutions
-export merge_subdomain_solutions
+# from EllipticPdeDomainDecomposition.jl,
+export prepare_global_schur,
+       apply_global_schur
+export prepare_local_schurs,
+       assemble_local_schurs,
+       apply_local_schur,
+       apply_local_schurs
 export assemble_A_ΓΓ_from_local_blocks
 
-export do_local_mass_covariance_assembly
-export do_local_mass_assembly
-export do_global_mass_reduced_assembly
-export do_global_mass_covariance_reduced_assembly
-export solve_local_kl
-export solve_global_reduced_kl
-export project_on_mesh
-export draw
-export draw!
-export trim_and_order
-export get_kl_coordinates
+# from EllipticPdeDomainDecomposition.jl,
+export get_schur_rhs,
+       get_subdomain_solutions,
+       merge_subdomain_solutions
 
+# from EllipticPdeDomainDecomposition.jl,
+export NeumannNeumannSchurPreconditioner,
+       prepare_neumann_neumann_schur_precond,
+       apply_neumann_neumann_schur
+
+# from EllipticPdeDomainDecomposition.jl,
+export NeumannNeumannInducedPreconditioner,
+       prepare_neumann_neumann_induced_precond,
+       apply_neumann_neumann_induced
+
+# from EllipticPdeDomainDecomposition.jl,
+export LorascPreconditioner,
+       prepare_lorasc_precond,
+       apply_lorasc
+
+# from EllipticPdeDomainDecomposition.jl,
+export DomainDecompositionLowRankPreconditioner,
+       prepare_domain_decomposition_low_rank_precond,
+       apply_inv_a0, apply_inv_a0!, apply_hmat,
+       apply_domain_decomposition_low_rank
+
+# from KarhunenLoeve.jl
+export do_mass_covariance_assembly
+export solve_kl
+
+# from KarhunenLoeveDomainDecomposition.jl
+export do_local_mass_covariance_assembly,
+       do_local_mass_assembly
+export solve_local_kl
+export do_global_mass_reduced_assembly,
+       do_global_mass_covariance_reduced_assembly,
+       solve_global_reduced_kl
+export draw,
+       draw!
+
+# from KarhunenLoevePllDomainDecomposition.jl, 
 export pll_do_global_mass_covariance_reduced_assembly
 export pll_solve_local_kl
 export pll_draw
 
+# from KarhunenLoeveDomainDecompositionHelper.jl
+export project_on_mesh
+export trim_and_order
+export get_kl_coordinates
 export suggest_parameters
 export get_root_filename
 
-export McmcSampler
-export prepare_mc_sampler
-export prepare_mcmc_sampler
+# from Samplers.jl,
+export McSampler,
+       prepare_mc_sampler
+export McmcSampler,
+       prepare_mcmc_sampler
 
+       
 include("Mesh.jl")
-include("EllipticPde.jl")
 include("BoundaryConditions.jl")
+include("EllipticPde.jl")
 include("EllipticPdeDomainDecomposition.jl")
 include("KarhunenLoeve.jl")
 include("KarhunenLoeveDomainDecomposition.jl")
