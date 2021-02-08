fadf287d475a929ebdd83e4c4ae019447d23f8be
Nicolas Venkovic <venkovic@moorcock.cerfacs.fr>
2021-02-08 12:26:30 +0100

    Keep postprocessing of Example09 up to date.

--- Example09_DefPcgMcmcStochasticEllipticPde.py
+++ Example09_DefPcgMcmcStochasticEllipticPde.py
@@ -1,20 +1,23 @@
 import numpy as np
 import pylab as pl
+from matplotlib.ticker import MaxNLocator
 from postproc_utils import get_root_filename
 
 #pl.rc('text', usetex=True)
 #pl.rcParams['text.usetex'] = True
 #pl.rcParams.update(params)
 
-tentative_nnode = 20_000
+tentative_nnode = 200_000
 model = 'SExp'
 L = .1
 sig2 = 1.
 root_fname = get_root_filename(model, sig2, L, tentative_nnode)
 
+nsmp_max = 4
+
 ndom = 20
 nvec = ndom + 5
-spdim = 2 * nvec
+spdim = int(2.5 * nvec)
 nbj = ndom
 
 it_eigdefpcg_amg = np.load("data/test01_%s_amg_0-eigdefpcg_nvec%d_spdim%d.it.npz" % (root_fname, nvec, spdim))
@@ -24,36 +27,40 @@ it_eigdefpcg_chol16 = np.load("data/test01_%s_chol16_0-eigdefpcg_nvec%d_spdim%d.
 
 nsmp, nchains = it_eigdefpcg_amg.shape
 
+nsmp = min(nsmp_max, nsmp)
+
 fig, axes = pl.subplots()
 
 axes.set_title(f'{tentative_nnode:,} DoFs, nvec = {nvec}, spdim = {spdim}')
 
 svals = [s for s in range(1, nsmp + 1)]
 
-axes.errorbar(svals, np.mean(it_eigdefpcg_bj, axis=1), yerr=np.std(it_eigdefpcg_bj, axis=1),
+axes.errorbar(svals, np.mean(it_eigdefpcg_bj[:nsmp, :], axis=1), yerr=np.std(it_eigdefpcg_bj[:nsmp, :], axis=1),
               label='bj%d-eigdefpcg' % nbj, 
               fmt='', lw=1,
               marker='s', ms=4,
               capsize=3, capthick=1.5,
               elinewidth=0.8)
-axes.errorbar(svals, np.mean(it_eigdefpcg_lorasc, axis=1), yerr=np.std(it_eigdefpcg_lorasc, axis=1),
+axes.errorbar(svals, np.mean(it_eigdefpcg_lorasc[:nsmp, :], axis=1), yerr=np.std(it_eigdefpcg_lorasc[:nsmp, :], axis=1),
               label='lorasc%d-eigdefpcg' % ndom,
               fmt='', lw=1,
               marker='^', ms=4,
               capsize=3, capthick=1.5,
               elinewidth=0.8)
-axes.errorbar(svals, np.mean(it_eigdefpcg_amg, axis=1), yerr=np.std(it_eigdefpcg_amg, axis=1),
+axes.errorbar(svals, np.mean(it_eigdefpcg_amg[:nsmp, :], axis=1), yerr=np.std(it_eigdefpcg_amg[:nsmp, :], axis=1),
               label='amg-eigdefpcg',
               fmt='', lw=1,
               marker='D', ms=4,
               capsize=3, capthick=1.5,
               elinewidth=0.8)
-axes.errorbar(svals, np.mean(it_eigdefpcg_chol16, axis=1), yerr=np.std(it_eigdefpcg_chol16, axis=1),
+axes.errorbar(svals, np.mean(it_eigdefpcg_chol16[:nsmp, :], axis=1), yerr=np.std(it_eigdefpcg_chol16[:nsmp, :], axis=1),
               label='chol16-eigdefpcg',
               fmt='', lw=1,
               marker='o', ms=4,
               capsize=3, capthick=1.5,
               elinewidth=0.8)
+
+axes.xaxis.set_major_locator(MaxNLocator(integer=True))
 
 axes.grid(linestyle='-.')
 axes.set_xlabel(r'$s$')
