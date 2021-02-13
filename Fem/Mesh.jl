18405d425c0d9b674d6f5a2e9e2f3edd5ab039a2
nvenkov1 <venkovic@gmail.com>
2021-02-13 23:40:31 +0100

    Added and cleaned comments in Mesh.jl.

--- Fem/Mesh.jl
+++ Fem/Mesh.jl
@@ -1,25 +1,88 @@
-using TriangleMesh
-using NPZ
+"""
+     function get_mesh(tentative_nnode::Int;
+                       keep_segments=true)
 
+Does parallel map from collection `coll` of subdomains with the 
+function `func`, doing a dynamic task scheduling over multiple hosts.
+
+Input:
+
+ `tentative_nnode::Int`,
+  tentative number of DoFs for the triangular mesh.
+
+ `keep_segments=true`
+
+Output:
+
+ `mesh::TriangleMesh.TriMesh`,
+  triangular mesh.
+
+"""
 function get_mesh(tentative_nnode::Int;
                   keep_segments=true)
                   
-  poly = polygon_unitSquare()
-  mesh = create_mesh(poly, voronoi=true, delaunay=true)
+  poly = TriangleMesh.polygon_unitSquare()
+  mesh = TriangleMesh.create_mesh(poly, voronoi=true, delaunay=true)
   divide_cell_into = ceil(Int, .645 * tentative_nnode)
-  mesh = refine(mesh, divide_cell_into=divide_cell_into, keep_segments=keep_segments)
+  mesh = TriangleMesh.refine(mesh, 
+                             divide_cell_into=divide_cell_into,
+                             keep_segments=keep_segments)
   return mesh
 end
 
+
+"""
+     function save_mesh(mesh::TriangleMesh.TriMesh,
+                        tentative_nnode::Int)
+
+Saves mesh data in NPZ files.
+
+Input:
+
+ `mesh::TriangleMesh.TriMesh`,
+  triangular mesh.
+
+ `tentative_nnode::Int`,
+  tentative number of DoFs for the triangular mesh.
+
+"""
 function save_mesh(mesh::TriangleMesh.TriMesh,
                    tentative_nnode::Int)
+
   npzwrite("data/DoF$tentative_nnode.cells.npz", mesh.cell' .- 1)
   npzwrite("data/DoF$tentative_nnode.points.npz", mesh.point')
   npzwrite("data/DoF$tentative_nnode.point_markers.npz", mesh.point_marker')
   npzwrite("data/DoF$tentative_nnode.cell_neighbors.npz", mesh.cell_neighbor')
 end
 
+
+"""
+     function load_mesh(tentative_nnode::Int)
+
+Loads mesh data from NPZ files.
+
+Input:
+
+ `tentative_nnode::Int`,
+  tentative number of DoFs for the triangular mesh.
+
+Output:
+
+ `cells::Array{Int,2}`, `size(cells) = (3, n_el)`,
+  nodes of each element.
+
+ `points::Array{Float64,2}`, `size(cells) = (2, nnode)`,
+  (x,y)-coordinates of DoFs.
+
+ `point_markers::Array{Int,1}`, `size(point_markers) = (nnode,)`,
+  indicates whether a node in Dirichlet (1) or not (0).
+
+ `cell_neighbors::Array{Int,2}`, `size(cell_neighbors) = (3, n_el)`,
+  neighboring elements of each element, or `-1` if edge is at mesh boundary. 
+
+"""
 function load_mesh(tentative_nnode::Int)
+
   cells = Array(npzread("data/DoF$tentative_nnode.cells.npz")') .+ 1
   points = Array(npzread("data/DoF$tentative_nnode.points.npz")')
   point_markers = Array(npzread("data/DoF$tentative_nnode.point_markers.npz")')
@@ -27,7 +90,29 @@ function load_mesh(tentative_nnode::Int)
   return cells, points, point_markers, cell_neighbors
 end
 
-function get_total_area(cells, points)
+
+"""
+     function get_total_area(cells::Array{Int,2},
+                             points::Array{Float64,2})
+
+Gets total area of trianular mesh.
+
+Input:
+
+ `cells::Array{Int,2}`, `size(cells) = (3, n_el)`,
+  nodes of each element.
+
+ `points::Array{Float64,2}`, `size(points) = (2, nnode)`,
+ (x,y)-coordinates of all mesh points.
+
+Output:
+ `area::Float64`,
+  total area of mesh.
+
+"""
+function get_total_area(cells::Array{Int,2}, 
+                        points::Array{Float64,2})
+
   _, nel = size(cells) # Number of elements
   area = 0. # Total mesh area
   x, y = zeros(3), zeros(3) # (x, y) coordinates of element vertices
@@ -59,7 +144,30 @@ function get_total_area(cells, points)
 end
 
 
+"""
+     function mesh_partition(cells::Array{Int,2}, ndom::Int)
+
+Gets mesh partition using metis.
+
+Input:
+
+ `cells::Array{Int,2}`, `size(cells) = (3, n_el)`,
+  nodes of each element.
+
+ `ndom::Int`,
+  number of subdomains.
+
+Output:
+
+ `epart::Array{Int,1}`, `size(epart) = (n_el,)`,
+  host subdomain of each element.
+
+ `npart::Array{Int,1}`, `size(npart) = (nnode,)`,
+  a host subdomain of each node.
+
+"""
 function mesh_partition(cells::Array{Int,2}, ndom::Int)
+
   _, nel = size(cells) # Number of elements
   nnode = maximum(cells) # Number of mesh nodes
 
@@ -87,48 +195,64 @@ function mesh_partition(cells::Array{Int,2}, ndom::Int)
 end
 
 
+"""
+     function save_partition(epart::Array{Int,1},
+                             npart::Array{Int,1},
+                             tentative_nnode::Int,
+                             ndom::Int)
+
+Saves data of mesh parition to NPZ files.
+
+Input:
+
+ `epart::Array{Int,1}`, `size(epart) = (n_el,)`,
+  host subdomain of each element.
+
+ `npart::Array{Int,1}`, `size(npart) = (nnode,)`,
+  a host subdomain of each node.
+
+ `tentative_nnode::Int`,
+  tentative number of DoFs for the triangular mesh.
+
+ `ndom::Int`,
+  number of subdomains.
+
+"""
 function save_partition(epart::Array{Int,1},
                         npart::Array{Int,1},
                         tentative_nnode::Int,
                         ndom::Int)
+
   npzwrite("data/DoF$tentative_nnode-ndom$ndom.epart.npz", epart .- 1)
   npzwrite("data/DoF$tentative_nnode-ndom$ndom.npart.npz", npart .- 1)
 end
 
 
-function load_partition(tentative_nnode::Int,
-                        ndom::Int)
+"""
+     function load_partition(tentative_nnode::Int, ndom::Int)
+
+Loads data of mesh parition from NPZ files.
+
+Input:
+
+ `tentative_nnode::Int`,
+  tentative number of DoFs for the triangular mesh.
+
+ `ndom::Int`,
+  number of subdomains.
+
+Output:
+
+ `epart::Array{Int,1}`, `size(epart) = (n_el,)`,
+  host subdomain of each element.
+
+ `npart::Array{Int,1}`, `size(npart) = (nnode,)`,
+  a host subdomain of each node.
+
+"""
+function load_partition(tentative_nnode::Int, ndom::Int)
+
   epart = Array(npzread("data/DoF$tentative_nnode-ndom$ndom.epart.npz")) .+ 1
   npart = Array(npzread("data/DoF$tentative_nnode-ndom$ndom.npart.npz")) .+ 1
   return epart, npart
 end
-
-
-"""
-using PyPlot
-
-function plot_TriMesh(m :: TriMesh; 
-                      linewidth :: Real = 1, 
-                      marker :: String = "None",
-                      markersize :: Real = 10,
-                      linestyle :: String = "-",
-                      color :: String = "red")
-
-    fig = matplotlib[:pyplot][:figure]("2D Mesh Plot", figsize = (10,10))
-    
-    ax = matplotlib[:pyplot][:axes]()
-    ax[:set_aspect]("equal")
-    
-    # Connectivity list -1 for Python
-    tri = ax[:triplot](m.point[1,:], m.point[2,:], m.cell'.-1 )
-    setp(tri, linestyle = linestyle,
-              linewidth = linewidth,
-              marker = marker,
-              markersize = markersize,
-              color = color)
-    
-    fig[:canvas][:draw]()
-    println("yrdy")    
-    return fig
-end
-"""
