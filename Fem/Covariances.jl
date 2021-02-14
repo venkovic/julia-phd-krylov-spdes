58680cbe01d799fac82723a7a43e9a3fb70a43f7
nvenkov1 <venkovic@gmail.com>
2021-02-14 22:51:15 +0100

    Added comments to Coavariances.jl

--- Fem/Covariances.jl
+++ Fem/Covariances.jl
@@ -1,9 +1,28 @@
-function cov_sexp(x1::Float64,
-                  y1::Float64,
-                  x2::Float64, 
-                  y2::Float64,
-                  sig2::Float64,
-                  L::Float64)
+"""
+     cov_sexp(x1::Float64, y1::Float64,
+              x2::Float64, y2::Float64,
+              sig2::Float64, L::Float64)
+
+Covariance of squared exponential model.
+
+Input:
+
+ `x1::Float64, y1::Float64`,
+  (x,y)-coordinates of first point.
+
+ `x2::Float64, y2::Float64`,
+  (x,y)-coordinates of second point.
+
+ `sig2::Float64`,
+  variance.
+  
+ `L::Float64`,
+  correlation length.
+
+"""
+function cov_sexp(x1::Float64, y1::Float64,
+                  x2::Float64, y2::Float64,
+                  sig2::Float64, L::Float64)
 
   return sig2 * exp(-((x1 - x2)^ 2 + (y1 - y2)^2) / L^2)
 end
