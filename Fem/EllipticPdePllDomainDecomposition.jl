3800f2ebf66c06498cc11006f2d4b3429785725a
nvenkov1 <venkovic@gmail.com>
2021-02-14 23:21:20 +0100

    Commented non-started work on EllipticPdePllDomainDecomposition.jl..

--- Fem/EllipticPdePllDomainDecomposition.jl
+++ Fem/EllipticPdePllDomainDecomposition.jl
@@ -1,16 +1,4 @@
-
-
-
-# Distributed computation of local KL expansion for each subdomain  
-@time domain = @sync @distributed merge! for idom in 1:ndom
-  relative_local, _ = suggest_parameters(mesh.n_point)
-  pll_solve_local_kl(mesh, epart, cov, nev, idom, 
-                     relative=relative_local)
-end
-
-
-
-
+"""
 function pll_apply_schur(A_IId::Array{SparseMatrixCSC{Float64,Int},1},
                          A_IΓd::Array{SparseMatrixCSC{Float64,Int},1},
                          A_ΓΓ::SparseMatrixCSC{Float64,Int},
@@ -26,6 +14,6 @@ Sx = @sync @distributed (+) for idom in 1:ndom
 end
 Sx .+= A_ΓΓ * x
 
-
 return Sx
 end
+"""
