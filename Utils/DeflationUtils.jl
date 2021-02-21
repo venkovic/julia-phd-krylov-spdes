778c13b240ae4b4a1270d157682eff12adcdcd69
nvenkov1 <venkovic@gmail.com>
2021-02-21 03:29:57 +0100

    Changed save_deflated_system().

--- Utils/DeflationUtils.jl
+++ Utils/DeflationUtils.jl
@@ -1,9 +1,9 @@
 """
      save_deflated_system(A::SparseMatrixCSC{Float64,Int},
                           b::Array{Float64,1},
-                          W::Array{Float64,2},
-                          s::Int,
-                          precond_id::String;
+                          W::Array{Float64,2};
+                          s=0,
+                          precond_id="",
                           print_error=false)
 
 Saves linear system and basis of deflation subspace to JLD files.
@@ -11,21 +11,31 @@ Saves linear system and basis of deflation subspace to JLD files.
 """
 function save_deflated_system(A::SparseMatrixCSC{Float64,Int},
                               b::Array{Float64,1},
-                              W::Array{Float64,2},
-                              s::Int,
-                              precond_id::String;
+                              W::Array{Float64,2};
+                              s=0,
+                              precond_id="",
                               print_error=false)
   
   I, J, V = findnz(A)
   
-  JLD.save("data/A_$(s)_I.jld", "I", I)
-  JLD.save("data/A_$(s)_J.jld", "J", J)
-  JLD.save("data/A_$(s)_V.jld", "V", V)
-  
-  JLD.save("data/b_$(s).jld", "b", b)
-  
-  JLD.save("data/W_$(precond_id)_$(s).jld", "W", W)
-  
+  if s == 0
+    JLD.save("data/A_I.jld", "I", I)
+    JLD.save("data/A_J.jld", "J", J)
+    JLD.save("data/A_V.jld", "V", V)
+    JLD.save("data/b.jld", "b", b)
+  else
+    JLD.save("data/A_$(s)_I.jld", "I", I)
+    JLD.save("data/A_$(s)_J.jld", "J", J)
+    JLD.save("data/A_$(s)_V.jld", "V", V)
+    JLD.save("data/b_$(s).jld", "b", b)
+  end
+   
+  if precond_id == "" && s == 0
+    JLD.save("data/W.jld", "W", W)
+  else
+    JLD.save("data/W_$(precond_id)_$(s).jld", "W", W)
+  end
+
   if print_error
     println("Warning: encountered a problem with eigdefpcg,
              data was saved for further investigation")
