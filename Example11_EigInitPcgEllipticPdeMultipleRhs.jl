3c38a53f9f5d4ed193c8a6b2357b9034a24f1e1f
nvenkov1 <venkovic@gmail.com>
2021-04-23 18:39:29 +0200

    Started working on Example11.

---
+++ Example11_EigInitPcgEllipticPdeMultipleRhs.jl
@@ -0,0 +1,148 @@
+push!(LOAD_PATH, "./Fem/")
+push!(LOAD_PATH, "./RecyclingKrylovSolvers/")
+push!(LOAD_PATH, "./MyPreconditioners/")
+push!(LOAD_PATH, "./Utils/")
+import Pkg
+Pkg.activate(".")
+
+using Fem
+using RecyclingKrylovSolvers: cg, pcg, defpcg, eigpcg, eigdefpcg
+using Preconditioners: AMGPreconditioner, SmoothedAggregation
+using MyPreconditioners: BJPreconditioner, BJop,
+                         Cholesky16, get_cholesky16,
+                         Cholesky32, get_cholesky32
+
+using Utils: space_println, printlnln, 
+             save_deflated_system, save_system,
+             load_system 
+
+using NPZ: npzread, npzwrite
+using Random: seed!
+using LinearMaps: LinearMap
+import SuiteSparse
+import JLD
+
+using LinearAlgebra: isposdef, rank
+
+include("Example09_DefPcgMcmcStochasticEllipticPde_Functions.jl")
+include("Example11_EigInitPcgEllipticPdeMultipleRhs_Functions.jl")
+
+
+troubleshoot = true
+
+maxit = 5_000
+tentative_nnode = 40_000
+load_existing_mesh = false
+
+ndom = 16
+load_existing_partition = false
+
+nbj = ndom
+
+nvec = floor(Int, 1.25 * ndom)
+spdim = 3 * ndom
+
+preconds = ["neumann-neumann$(ndom)_0"] # ∈ ("amg_0", 
+                                        #    "bj$(nbj)_0",
+                                        #    "lorasc$(ndom)_0",
+                                        #    "lorasc$(ndom)_1",
+                                        #    "neumann-neumann$(ndom)_0")
+
+const preconds_with_dd = ("lorasc$(ndom)_0",
+                          "lorasc$(ndom)_1",
+                          "neumann-neumann$(ndom)_0")
+
+nchains = 50
+nsmp = 5
+seed!(481_456)
+
+model = "SExp"
+sig2 = 1.
+L = .1
+
+root_fname = get_root_filename(model, sig2, L, tentative_nnode)
+
+function f(x::Float64, y::Float64)
+  return -1.
+end
+  
+function uexact(x::Float64, y::Float64)
+  return .734
+end
+
+#
+# Load mesh
+#
+if load_existing_mesh
+  cells, points, point_markers, cell_neighbors = load_mesh(tentative_nnode)
+else
+  mesh = get_mesh(tentative_nnode)
+  cells = mesh.cell
+  points = mesh.point
+  point_markers = mesh.point_marker
+  cell_neighbors = mesh.cell_neighbor
+end
+
+dirichlet_inds_g2l, not_dirichlet_inds_g2l,
+dirichlet_inds_l2g, not_dirichlet_inds_l2g =
+get_dirichlet_inds(points, point_markers)
+
+n = not_dirichlet_inds_g2l.count
+nnode = mesh.n_point
+
+space_println("nnode = $(size(points)[2])")
+space_println("nel = $(size(cells)[2])")
+
+#
+# Load mesh parition
+#
+if load_existing_partition
+  epart, npart = load_partition(tentative_nnode, ndom)
+else
+  epart, npart = mesh_partition(cells, ndom)
+  save_partition(epart, npart, tentative_nnode, ndom)
+end
+
+ind_Id_g2l, ind_Γd_g2l, ind_Γ_g2l, ind_Γd_Γ2l, node_owner,
+elemd, node_Γ, node_Γ_cnt, node_Id, nnode_Id = set_subdomains(cells,
+                                                              cell_neighbors,
+                                                              epart,
+                                                              npart,
+                                                              dirichlet_inds_g2l)
+
+#
+# Assembly of constant preconditioners
+#
+g_0 = zeros(Float64, nnode)
+
+printlnln("do_isotropic_elliptic_assembly for ξ = 0 ...")
+A_0, _ = @time do_isotropic_elliptic_assembly(cells, points,
+                                              dirichlet_inds_g2l,
+                                              not_dirichlet_inds_g2l,
+                                              point_markers,
+                                              exp.(g_0), f, uexact)
+
+#
+# Load kl representation
+# 
+M = get_mass_matrix(cells, points)
+Λ = npzread("data/$root_fname.kl-eigvals.npz")
+Ψ = npzread("data/$root_fname.kl-eigvecs.npz")
+
+#
+# Prepare constant preconditioners
+#
+Π, Π_IId = get_constant_preconds(preconds, A_0)
+
+#
+# Run test
+#
+iters = test_solvers_on_several_chains(nchains, nsmp, Λ, Ψ, Π, 
+                                       preconds, nvec, spdim, maxit,
+                                       do_pcg=false,
+                                       do_eigpcg=false,
+                                       do_eigdefpcg=true,
+                                       do_defpcg=false,
+                                       save_results=true,
+                                       troubleshoot=troubleshoot,
+                                       Π_IId=Π_IId)
---
+++ Example11_EigInitPcgEllipticPdeMultipleRhs_Functions.jl
@@ -0,0 +1,318 @@
+using SparseArrays: SparseMatrixCSC
+using LinearMaps: FunctionMap
+
+"""
+test_solvers_on_multiple_rhs(nsmp::Int,
+                             Λ::Array{Float64,1},
+                             Ψ::Array{Float64,2}
+                             Π,
+                             precond_labels::Array{String,1},
+                             do_pcg::Bool,
+                             do_eigpcg::Bool,
+                             do_eigdefpcg::Bool,
+                             do_defpcg::Bool,
+                             nvec::Int,
+                             spdim::Int;
+                             verbose=true,
+                             save_results=false)
+
+
+"""
+function test_solvers_on_multiple_rhs(nsmp::Int,
+                                      Λ::Array{Float64,1},
+                                      Ψ::Array{Float64,2},
+                                      Π,
+                                      preconds::Array{String,1},
+                                      nvec::Int,
+                                      spdim::Int,
+                                      maxit::Int;
+                                      do_pcg=true,
+                                      do_initpcg=true,
+                                      do_eigpcg=true,
+                                      do_eigdefpcg=true,
+                                      do_defpcg=true,
+                                      verbose=true,
+                                      save_results=false,
+                                      troubleshoot=false,
+                                      Π_IId=nothing)
+
+  status = 0 
+
+  sampler = prepare_mc_sampler(Λ, Ψ)
+
+  verbose ? println("\n1 / $nsmp") : nothing
+  verbose ? print("do_isotropic_elliptic_assembly ... ") : nothing
+  Δt = @elapsed A, b = do_isotropic_elliptic_assembly(cells, points,
+                                                      dirichlet_inds_g2l,
+                                                      not_dirichlet_inds_g2l,
+                                                      point_markers,
+                                                      exp.(sampler.g),
+                                                      f, uexact)
+  verbose ? println("$Δt seconds") : nothing
+
+  do_schur = false
+  for precond in preconds
+    occursin("neumann-neumann", precond) ? do_schur = true : nothing
+  end
+
+  if do_schur
+
+
+    # where are epart, ... ? Are these global :/ ?
+     
+    verbose ? printlnln("prepare_local_schurs ...") : nothing
+    Δt = @elapsed A_IIdd, A_IΓdd, A_ΓΓdd, b_Idd, b_Γ = prepare_local_schurs(cells,
+                                                                            points,
+                                                                            epart,
+                                                                            ind_Id_g2l,
+                                                                            ind_Γd_g2l,
+                                                                            ind_Γ_g2l,
+                                                                            node_owner,
+                                                                            a_vec,
+                                                                            f,
+                                                                            uexact)
+    verbose ? println("$Δt seconds") : nothing
+
+    S, b_schur = do_condensed_isotropic_elliptic_assembly(ndom,
+                                                          A_IIdd,
+                                                          A_IΓdd,
+                                                          A_ΓΓdd,
+                                                          b_Idd,
+                                                          b_Γ,
+                                                          ind_Γ_g2l,
+                                                          ind_Γd_Γ2l,
+                                                          node_Γ_cnt,
+                                                          Π_IId)
+
+    n_Γ, = size(b_schur)
+    x_schur = zeros(Float64, n_Γ)
+    WtS = Array{Float64,2}(undef, nvec, n_Γ)
+  end
+
+  methods = String[]
+  W = Dict{String,Array{Float64,2}}()
+  WtA = Array{Float64,2}(undef, nvec, A.n)
+  iter = Dict{String,Array{Int,1}}()
+
+  for precond in preconds
+    if do_pcg
+      method = precond * "-pcg"
+      push!(methods, method)
+      iter[method] = Array{Int,1}(undef, nsmp)
+    end
+
+    if do_initpcg
+        method = precond * "-initpcg"
+        push!(methods, method)
+        iter[method] = Array{Int,1}(undef, nsmp)
+    end
+
+    if do_eigpcg 
+      method = precond * "-eigpcg"
+      push!(methods, method)
+      iter[method] = Array{Int,1}(undef, nsmp)
+      W[method] = Array{Float64,2}(undef, A.n, nvec)
+    end
+
+    if do_eigdefpcg
+      method = precond * "-eigdefpcg"
+      push!(methods, method)
+      iter[method] = Array{Int,1}(undef, nsmp)
+      W[method] = Array{Float64,2}(undef, A.n, nvec)
+    end
+
+    if do_defpcg
+      method = precond * "-defpcg"
+      push!(methods, method)
+      iter[method] = Array{Int,1}(undef, nsmp)
+      W[method] = Array{Float64,2}(undef, A.n, nvec)
+    end
+  end
+
+  x = zeros(Float64, A.n)
+
+  #
+  # Sample rhs uniformly at random and solve linear systems with 
+  # online eigenvectors approximation
+  #
+  it = 0
+  for s in 1:nsmp
+
+    if s > 1
+      verbose ? print("\n$s / $nsmp") : nothing
+
+      b = rand(A.n)
+
+      if do_schur
+
+        domain_decompose_rhs!(b, b_Idd, b_Γ, 
+                              ind_Id_g2l,
+                              ind_Γ_g2l,
+                              node_owner)
+
+        b_schur = get_schur_rhs(b_Idd, A_IIdd, A_IΓdd, b_Γ, ind_Γd_Γ2l, preconds=Π_IId)
+      end
+    end # if s > 1
+
+    for (p, precond) in enumerate(preconds)
+
+      if do_pcg
+        method = precond * "-pcg"
+        verbose ? print("$method of A * u = b ... ") : nothing
+        troubleshoot ? save_system(A, b) : nothing 
+        try
+          if occursin("neumann-neumann", precond)
+            x_schur .= 0
+            Δt = @elapsed _, it, _ = pcg(S, b_schur, x_schur, Π[p], maxit=maxit)
+          else
+            x .= 0
+            Δt = @elapsed _, it, _  = pcg(A, b, x, Π[p], maxit=maxit)
+          end
+        catch err
+          status = -1
+          return iter, status
+        end
+        verbose ? println("$Δt seconds, iter = $it") : nothing
+        iter[method][s] = it
+        flush(stdout)
+      end
+
+
+      if do_initpcg
+      end
+
+      if do_eigpcg
+        method = precond * "-eigpcg"
+        verbose ? print("$method of A * u = b ... ") : nothing
+        troubleshoot ? save_system(A, b) : nothing 
+        try
+          if occursin("neumann-neumann", precond)
+            if s == 1
+              x_schur .= 0
+            else
+              if isa(S, FunctionMap)
+                for j in 1:nvec
+                  WtS[j, :] .= S * W[method][:, j] 
+                end
+              else
+                WtS .= W[method]'S
+              end
+              H = WtS * W[method]
+              x_schur .= W[method] * (H \ (W[method]'b_schur))
+            end
+            Δt = @elapsed _, it, _, W[method] = eigpcg(S, b_schur, x_schur, Π[p], nvec, spdim, maxit=maxit)
+          else
+            if s == 1
+              x .= 0
+            else
+              WtA .= W[method]'A
+              H = WtA * W[method]
+              x .= W[method] * (H \ (W[method]'b))
+            end
+            Δt = @elapsed _, it, _, W[method] = eigpcg(A, b, x, Π[p], nvec, spdim, maxit=maxit)
+          end
+        catch err
+          throw(err)
+          status = -1
+          return iter, status
+        end
+        verbose ? println("$Δt seconds, iter = $it") : nothing
+        iter[method][s] = it
+        flush(stdout)
+      end # if do_eigpcg
+
+      if do_eigdefpcg
+        method = precond * "-eigdefpcg"
+        verbose ? print("$method of A * u = b ... ") : nothing
+        if s == 1
+          troubleshoot ? save_system(A, b) : nothing
+
+          #Δt = @elapsed _, it, _, W[method] = eigpcg(S, b_schur, x, Π[p], nvec, spdim, maxit=maxit)
+          try
+            if occursin("neumann-neumann", precond)
+              x_schur .= 0
+              Δt = @elapsed _, it, _, W[method] = eigpcg(S, b_schur, x_schur, Π[p], nvec, spdim, maxit=maxit)
+            else
+              x .= 0
+              Δt = @elapsed _, it, _, W[method] = eigpcg(A, b, x, Π[p], nvec, spdim, maxit=maxit)
+            end
+          catch err
+            status = -1
+            println(err)
+            return iter, status
+          end
+        else
+          troubleshoot ? save_deflated_system(A, b, W[method]) : nothing
+          try
+            if rank(W[method]) < .9 * nvec
+              status = -1
+              return iter, status
+            end
+            if occursin("neumann-neumann", precond)
+              x_schur .= 0
+              Δt = @elapsed _, it, _, W[method] = eigdefpcg(S, b_schur, x_schur, Π[p], W[method], spdim, maxit=maxit)
+            else
+              x .= 0
+              Δt = @elapsed _, it, _, W[method] = eigdefpcg(A, b, x, Π[p], W[method], spdim, maxit=maxit)
+            end
+          catch err
+            status = -1
+            return iter, status
+          end
+        end
+        verbose ? println("$Δt seconds, iter = $it") : nothing
+        iter[method][s] = it
+        flush(stdout)
+      end # if do_eigdefpcg
+
+      if do_defpcg
+        method = precond * "-defpcg"
+        x .= 0
+        verbose ? print("$method of A * u = b ... ") : nothing
+        if s == 1
+          # Some work remains to do here
+          troubleshoot ? save_system(A, b) : nothing 
+          try
+            if occursin("neumann-neumann", precond)
+              x_schur .= 0
+              Δt = @elapsed _, it, _, W[method] = eigpcg(S, b_schur, x_schur, Π[p], nvec, spdim, maxit=maxit)
+            else
+              x .= 0
+              Δt = @elapsed _, it, _, W[method] = eigpcg(A, b, x, Π[p], nvec, spdim, maxit=maxit)
+            end
+          catch err
+            status = -1
+            return iter, status
+          end
+
+        else
+          troubleshoot ? save_deflated_system(A, b, W[method]) : nothing 
+          try
+            if occursin("neumann-neumann", precond)
+              x_schur .= 0
+              Δt = @elapsed _, it, _, W[method] = defpcg(S, b_schur, W[method], x_schur, Π[p], maxit=maxit)
+            else
+              x .= 0
+              Δt = @elapsed _, it, _, W[method] = defpcg(A, b, W[method], x, Π[p], maxit=maxit)
+            end            
+          catch err
+            status = -1
+            return iter, status
+          end
+        end
+        verbose ? println("$Δt seconds, iter = $it") : nothing
+        iter[method][s] = it
+        flush(stdout)
+      end # if do_defpcg
+
+    end # for (p, precond)
+  end # for s in 1:nsmp
+
+  if save_results
+    for method in methods
+      npzwrite("data/$(root_fname)_$(method)_mrhs_.it.npz", iter[method])
+    end
+  end
+
+  return iter, status
+end
