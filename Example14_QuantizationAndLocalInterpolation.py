f5a1d3692289c40e13c8263e1548d2540e75675d
nvenkov1 <venkovic@gmail.com>
2021-04-26 16:24:29 +0200

    Example14 is for Quantization with local interpolation.

---
+++ Example14_QuantizationAndLocalInterpolation.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+distance = ('L2-full', 'cdf-full',)
+Ps = (5, 10, 20, 30, 40, 50, 60, 70 ,80, 90, 100,)
+nsmp_preconds = 10_000
+nsmp = 1_000
+root_fname = 'SExp_sig21.0_L0.1_DoF4000'
+
+iters, assignments, dists, dists_to_0 = {}, {}, {}, {}
+
+for dist in distance:
+  iters[dist] = np.zeros((nsmp, len(Ps)), dtype=int)
+  assignments[dist] = np.zeros((nsmp, len(Ps)), dtype=int)
+  dists[dist] = np.zeros((nsmp, len(Ps)))
+  dists_to_0[dist] = np.zeros((nsmp, len(Ps)))
+
+  for (p, P) in enumerate(Ps):
+    iters[dist][:, p] = np.load(f"data/Example12_{root_fname}_{dist}_{P}_{nsmp_preconds}.iters.npz")
+    assignments[dist][:, p] = np.load(f"data/Example12_{root_fname}_{dist}_{P}_{nsmp_preconds}.assignments.npz")
+    dists[dist][:, p] = np.load(f"data/Example12_{root_fname}_{dist}_{P}_{nsmp_preconds}.dists.npz")
+    dists_to_0[dist][:, p] = np.load(f"data/Example12_{root_fname}_{dist}_{P}_{nsmp_preconds}.dists_to_0.npz")
+
+mean_iters, std_iters = {}, {}
+for dist in distance:
+  mean_iters[dist] = np.mean(iters[dist], axis=0)
+  std_iters[dist] = np.std(iters[dist], axis=0)
+
+import pylab as pl
+fig, axes = pl.subplots(1, 2, sharey=True)
+axes[0].errorbar(Ps, mean_iters[distance[0]], yerr=std_iters[distance[0]], fmt='o')
+axes[1].errorbar(Ps, mean_iters[distance[1]], yerr=std_iters[distance[1]], fmt='o')
+axes[0].set_ylim(50, 150)
+for ax in axes: ax.grid(ls='-.')
+pl.show()
---
+++ Example14_QuantizationAndLocalInterpolation_Functions.jl
@@ -0,0 +1,161 @@
+using Clustering
+using Distributions: MvNormal, Normal, cdf
+using Statistics: quantile
+using Random: seed!
+
+"""
+     get_quantizer(n::Int,
+                   P::Int,
+                   Λ::Array;
+                   distance="L2-full")
+Input:
+ `n::Int`,
+  number of of points from the stochastic space.
+
+Output:
+ `X::Array{Float64,2}`, `size(X) = (length(Λ), n)`,
+  (non-weighted) coordinates in stochastic space.
+
+ `centroids::Array{Float64,2}`, `size(centers) = (length(Λ), P)`,
+  (non-weighted) coordinates of cluster centers in stochastic space.
+
+ `assignments::Array{Int,1}`, `length(assignments) = n`,
+  cluster to which each point in X is assigned.
+
+ `costs::Array{Float64,1}`, `length(costs) = n`,
+  cost of each point wrt to its center. 
+
+"""
+function get_quantizer(n::Int,
+                       P::Int,
+                       Λ::Array;
+                       distance="L2-full")
+  nKL = length(Λ)
+
+  seed!(987_654_321)
+  X = rand(MvNormal(nKL, 1.), n)
+
+  if distance == "L2-full"
+    X .*= sqrt.(Λ) 
+    res = kmeans(X, P)
+    X ./= sqrt.(Λ)
+    res.centers ./= sqrt.(Λ)
+
+  elseif distance == "cdf-full"
+    X .= cdf.(Normal(), X) 
+    res = kmeans(X, P)
+    X .= quantile.(Normal(), X)
+    res.centers .= quantile.(Normal(), res.centers)
+  end
+
+  return X, res.centers, res.assignments, res.costs
+end
+
+
+"""
+     get_centroidal_preconds(preconds::Array{String,1},
+                             centroids::Array{Float64,2},
+                             Λ::Array{Float64,1},
+                             Ψ::Array{Float64,2})
+
+Prepares centroidal preconditioners of Voronoi quantizer.
+
+Input:
+ `centroids::Array{Float64,2}`,
+  (non-weighted) centroidal coordinates in stochastic space.
+
+ `Λ::Array{Float64,1}`,
+  eigenvalues of KL expansion.
+
+ `Ψ::Array{Float64,2}`,
+  nodal values of eigevectors of KL expansion.
+
+Output:
+ `Π`,
+  list of constant AMG preconditioners
+
+"""
+function get_centroidal_preconds(centroids::Array{Float64,2},
+                                 Λ::Array{Float64,1},
+                                 Ψ::Array{Float64,2})
+
+  nKL, ncentroids = size(centroids)
+  sampler = prepare_mc_sampler(Λ, Ψ)
+
+  Π = []
+
+  for p in 1:ncentroids
+
+    # assemble operator 
+    set!(sampler, centroids[:, p])
+    A, _ = do_isotropic_elliptic_assembly(cells, points,
+                                          dirichlet_inds_g2l,
+                                          not_dirichlet_inds_g2l,
+                                          point_markers,
+                                          exp.(sampler.g), f, uexact)
+        
+    # build preconditioner
+    println("prepare amg preconditioner of centroid $p / $(ncentroids) ...")
+    Π_amg = @time AMGPreconditioner{SmoothedAggregation}(A);
+    push!(Π, Π_amg)
+    flush(stdout)
+
+  end
+  
+  return Π
+end # function get_centroidal_preconds
+
+
+function test_solver_with_centroidal_preconds(nsmp,
+                                              Π,
+                                              centroids::Array{Float64,2},
+                                              Λ::Array{Float64,1},
+                                              Ψ::Array{Float64,2};
+                                              maxit=5_000)
+
+  nKL, ncentroids = size(centroids)
+  sampler = prepare_mc_sampler(Λ, Ψ)
+
+  seed!(123_456_789)
+  X = rand(MvNormal(nKL, 1.), nsmp)
+
+  assignments = zeros(Int, nsmp)
+  iters = zeros(Int, nsmp)
+  dists = zeros(Float64, nsmp)
+  dists_to_0 = zeros(Float64, nsmp)
+
+  Δx = Array{Float64,1}(undef, nKL)
+  dist = Array{Float64,1}(undef, ncentroids)
+
+  for ismp in 1:nsmp
+  
+    # assemble operator 
+    set!(sampler, X[:, ismp])
+    A, b = do_isotropic_elliptic_assembly(cells, points,
+                                          dirichlet_inds_g2l,
+                                          not_dirichlet_inds_g2l,
+                                          point_markers,
+                                          exp.(sampler.g), f, uexact)  
+
+    # find closest centroid
+    for p in 1:ncentroids
+      Δx = (centroids[:, p] .- X[:, ismp]) .* sqrt.(Λ)
+      dist[p] = sqrt(Δx'Δx)
+    end
+    assignments[ismp] = argmin(dist)
+    dists[ismp] = dist[assignments[ismp]]
+    Δx = X[:, ismp] .* sqrt.(Λ)
+    dists_to_0[ismp] = sqrt(Δx'Δx)
+
+
+    # solve system with closest constant preconditioner
+    x = zeros(Float64, A.n)
+    _, iters[ismp], _  = pcg(A, b, x, Π[assignments[ismp]], maxit=maxit)
+
+    println("done solving system $ismp / $(nsmp) with $(iters[ismp]) iters.")
+
+  end # for ismp in 1:nsmp
+
+  return assignments, iters, dists, dists_to_0
+end
+
