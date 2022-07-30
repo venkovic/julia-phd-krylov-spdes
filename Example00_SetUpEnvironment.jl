6effd046af5b25b1db20ced590fa4eb49f2800f1
nicolas <venkovic@cerfacs.fr>
2022-07-30 02:32:49 +0200

    Changed cluster to hector.

--- Example00_SetUpEnvironment.jl
+++ Example00_SetUpEnvironment.jl
@@ -1,9 +1,8 @@
 using Distributed
 
-addprocs(([("marcel", 1)]), tunnel=true)
-addprocs(([("andrew", 1)]), tunnel=true)
-addprocs(([("hector", 1)]), tunnel=true)
-addprocs(([("lucien", 1)]), tunnel=true)
+addprocs(([("hector0", 1)]), tunnel=true)
+addprocs(([("hector1", 1)]), tunnel=true)
+addprocs(([("hector2", 1)]), tunnel=true)
 
 @everywhere begin
   import Pkg
