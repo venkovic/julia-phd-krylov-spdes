d0b051f266551a2421f1e5c3049725b3ee6f4ec3
nicolas <venkovic@gmail.com>
2022-12-01 12:05:43 +0100

    Updated Example20.

--- Example20_QuantizedPreconditioner.jl
+++ Example20_QuantizedPreconditioner.jl
@@ -16,6 +16,7 @@ import AlgebraicMultigrid
 using Preconditioners: AMGPreconditioner, SmoothedAggregation
 using NPZ: npzread, npzwrite
 using Random: seed!; seed!(123_456);
+using Distributions: Normal, quantile
 using LinearMaps: LinearMap, cholesky
 using SparseArrays: SparseMatrixCSC
 using Distributions: Normal
@@ -23,7 +24,7 @@ using Distributions: Normal
 include("Example20_QuantizedPreconditioner_Functions.jl")
 
 tentative_nnode = 100_000
-load_existing_mesh = false
+load_existing_mesh = true
 
 nreals = 100_000
 
@@ -34,14 +35,20 @@ root_fname = get_root_filename(model, sig2, L, tentative_nnode)
 
 precond = "k-means" # precond ∈ ("k-means", "deterministic-grid")
 
+s = 2 * quantile(Normal(0, 1), 2/3)
+
 if precond == "k-means"
   ns = 100_000
-  P = 10 # P ∈ (10, 100, 1_000, 10_000,)
-  dist = "L2" # dist ∈ ("L2", "cdf",)
-  nKL_trunc = 8 # _nKL ∈ [8, 24, 48, 170]
+  P = 10_000 # P ∈ (10, 100, 1_000, 10_000,)
+  dist = "cdf" # dist ∈ ("L2", "cdf",)
+  nKL_trunc = 170 # nKL_trunc ∈ [8, 24, 48, 170]
 elseif precond == "deterministic-grid"
-  m = 1 # m ∈ (0, 1, ...,)
-  P = 1 + 2^m
+  m = 0 # m ∈ (0, 1, ..., 13)
+  if m == 0
+    P = 1
+  else
+    P = 1 + 2^m
+  end
   nKL_trunc = m
 end
 
@@ -67,10 +74,6 @@ function uexact(xx::Float64, yy::Float64)
   return .734
 end
 
-println()
-space_println("nnode = $(size(points)[2])")
-space_println("nel = $(size(cells)[2])")
-
 M = get_mass_matrix(cells, points)
 Λ = npzread("data/$root_fname.kl-eigvals.npz")
 Ψ = npzread("data/$root_fname.kl-eigvecs.npz")
@@ -80,14 +83,18 @@ nKL = length(Λ)
 if precond == "k-means"
   hηt_kmeans, hξt_kmeans = do_kmeans(ns, P, dist, nKL_trunc, Λ)
 elseif precond == "deterministic-grid"
-  hηt_kmeans, hξt_kmeans = get_deterministic_grid(P, nKL_trunc, Λ)
+  hηt_kmeans, hξt_kmeans = get_deterministic_grid(P, nKL_trunc, Λ, s)
 end
 
 # Sample and attribute realizations
 Ξ = rand(Normal(), nKL, nreals)
 which = Array{Int,1}(undef, nreals)
-for ireal in 1:nreals  
-  which[ireal] = find_precond(scale_data(Ξ[:, ireal], Λ, dist), hηt_kmeans)
+for ireal in 1:nreals
+  if precond == "k-means"
+    which[ireal] = find_precond(scale_data(Ξ[:, ireal], Λ, dist), hηt_kmeans)
+  elseif precond == "deterministic-grid"
+    which[ireal] = find_precond(scale_data(Ξ[:, ireal], Λ, "L2"), hηt_kmeans)
+  end
 end
 
 # Save realizations
@@ -99,14 +106,14 @@ for p in 1:P
   end
 end
 
-# Test preconditioners
-ξ = Array{Float64,1}(undef, nKL)
+ξ, g = draw(Λ, Ψ)
+
+
 for p in 1:P
   ξ[1:nKL_trunc] = hξt_kmeans[:, p]
   ξ[nKL_trunc+1:end] .= 0
   set!(Λ, Ψ, ξ, g)
 
-  printlnln("do_isotropic_elliptic_assembly for preconditioner p = $p / $P ...")
   hAt, _ = @time do_isotropic_elliptic_assembly(cells, points,
                                                 dirichlet_inds_g2l,                                                
                                                 not_dirichlet_inds_g2l,
@@ -116,10 +123,10 @@ for p in 1:P
   M_amg = AMGPreconditioner{SmoothedAggregation}(hAt)
   M_chol = cholesky(hAt)
 
-  if precond == "k-means"
-    Ξ = npzread("data/Example20.dist$dist.nKL$nKL_trunc.P$P.p$p.reals.npz")
-  elseif
-    Ξ = npzread("data/Example20.m$m.P$P.p$p.reals.npz")
+  Ξ = if precond == "k-means"
+    npzread("data/Example20.dist$dist.nKL$nKL_trunc.P$P.p$p.reals.npz") 
+  elseif precond == "deterministic-grid"
+    npzread("data/Example20.m$m.P$P.p$p.reals.npz")
   end
   _, nreals = size(Ξ)
   
@@ -127,22 +134,20 @@ for p in 1:P
   iters_chol = Array{Int,1}(undef, nreals)
 
   for ireal in 1:nreals
-    set!(Λ, Ψ, Ξ[:, ireal], g)
+    printlnln("working on realization $ireal / $nreals for p = $p / $P ...")
+    @time begin
+      set!(Λ, Ψ, Ξ[:, ireal], g)
 
-    printlnln("do_isotropic_elliptic_assembly  ireal = $ireal / $nreals (p = $p / $P) ...")
-    A, b = @time do_isotropic_elliptic_assembly(cells, points,
-                                                dirichlet_inds_g2l,
-                                                not_dirichlet_inds_g2l,
-                                                point_markers,
-                                                exp.(g), f, uexact)
+      A, b = do_isotropic_elliptic_assembly(cells, points,
+                                            dirichlet_inds_g2l,
+                                            not_dirichlet_inds_g2l,
+                                            point_markers,
+                                            exp.(g), f, uexact)
                                                 
-    printlnln("pcg solve of A * u = b with Π_amg ...")
-    _, iters_amg[ireal], _ = @time pcg(A, b, zeros(A.n), M_amg)
-    println("it = $(iters_amg[ireal])")
+      _, iters_amg[ireal], _ = pcg(A, b, zeros(A.n), M_amg)
     
-    printlnln("pcg solve of A * u = b with chol ...")
-    _, iters_chol[ireal], _ = @time pcg(A, b, zeros(A.n), M_chol)
-    println("it = $(iters_chol[ireal])")
+      _, iters_chol[ireal], _ = pcg(A, b, zeros(A.n), M_chol)
+    end
   end
   
   if precond == "k-means"
@@ -153,6 +158,7 @@ for p in 1:P
     npzwrite("data/Example20.m$m.P$P.p$p.chol.iters.npz", iters_chol)
   end
 end
+
 
 
 
--- Example20_QuantizedPreconditioner_Functions.jl
+++ Example20_QuantizedPreconditioner_Functions.jl
@@ -54,24 +54,99 @@ function find_precond(x::Vector{Float64}, hXt::Array{Float64,2})
 end
 
 function do_kmeans(ns, P, dist, nKL, Λ)
-    hηt_kmeans = zeros(nKL, P)
-    hξt_kmeans = zeros(nKL, P)
+  hηt_kmeans = zeros(nKL, P)
+  hξt_kmeans = zeros(nKL, P)
 
-    Λtrunc = Λ[1:nKL]
-    X = get_scaled_data(ns, Λtrunc, dist)
-    println("kmeans working on nKL = $nKL, dist = $dist.")
-    res = kmeans(X, P, tol=1e-8, maxiter=1_000)
-    hηt_kmeans = res.centers
-    hξt_kmeans = copy(hηt_kmeans)
-    if dist == "L2"
-      hξt_kmeans ./= sqrt.(Λtrunc)
-    elseif dist == "cdf"
-      hξt_kmeans ./= sqrt.(Λtrunc)
-      hξt_kmeans .= quantile.(Normal(), hξt_kmeans)
-    end
-    return hηt_kmeans, hξt_kmeans
+  Λtrunc = Λ[1:nKL]
+  X = get_scaled_data(ns, Λtrunc, dist)
+  println("kmeans working on nKL = $nKL, dist = $dist.")
+  res = kmeans(X, P, tol=1e-8, maxiter=1_000)
+  hηt_kmeans = res.centers
+  hξt_kmeans = copy(hηt_kmeans)
+  if dist == "L2"
+    hξt_kmeans ./= sqrt.(Λtrunc)
+  elseif dist == "cdf"
+    hξt_kmeans ./= sqrt.(Λtrunc)
+    hξt_kmeans .= quantile.(Normal(), hξt_kmeans)
   end
+  return hηt_kmeans, hξt_kmeans
+ end
 
-function get_deterministic_grid()
-  nothing  
+function get_deterministic_grid(P, nKL, Λ, s)
+  hξt_kmeans = Array{Float64,2}(undef, nKL, P)
+  p = 0  
+  hξt_kmeans[:, p+1] .= 0.
+  if nKL > 0
+    for p in 1:2^nKL
+      bits = bitstring(p)[end-(nKL-1):end]
+      for k in 1:nKL
+        if bits[k] == '0'
+          hξt_kmeans[k, p+1] = -s
+        else
+          hξt_kmeans[k, p+1] = s
+        end
+      end
+    end
+  end
+  hηt_kmeans = sqrt.(Λ[1:nKL]) .* hξt_kmeans
+  return hηt_kmeans, hξt_kmeans
 end
+
+
+function test_preconds()
+  ξ, g = draw(Λ, Ψ)
+
+  Threads.@threads for p in 1:P
+    ξ[1:nKL_trunc] = hξt_kmeans[:, p]
+    ξ[nKL_trunc+1:end] .= 0
+    set!(Λ, Ψ, ξ, g)
+
+    printlnln("do_isotropic_elliptic_assembly for preconditioner p = $p / $P ...")
+    hAt, _ = @time do_isotropic_elliptic_assembly(cells, points,
+                                                  dirichlet_inds_g2l,                                                
+                                                  not_dirichlet_inds_g2l,
+                                                  point_markers,
+                                                  exp.(g), f, uexact)
+
+    M_amg = AMGPreconditioner{SmoothedAggregation}(hAt)
+    M_chol = cholesky(hAt)
+
+    if precond == "k-means"
+      Ξ = npzread("data/Example20.dist$dist.nKL$nKL_trunc.P$P.p$p.reals.npz")
+    elseif precond == "determistic-grid"
+      Ξ = npzread("data/Example20.m$m.P$P.p$p.reals.npz")
+    end
+    _, nreals = size(Ξ)
+  
+    iters_amg = Array{Int,1}(undef, nreals)
+    iters_chol = Array{Int,1}(undef, nreals)
+
+    for ireal in 1:nreals
+      set!(Λ, Ψ, Ξ[:, ireal], g)
+
+      printlnln("working on realization $ireal / $nreals for p = $p / $P ...")
+      A, b = @time do_isotropic_elliptic_assembly(cells, points,
+                                                  dirichlet_inds_g2l,
+                                                  not_dirichlet_inds_g2l,
+                                                  point_markers,
+                                                  exp.(g), f, uexact)
+                                                
+      _, iters_amg[ireal], _ = @time pcg(A, b, zeros(A.n), M_amg)
+    
+      _, iters_chol[ireal], _ = @time pcg(A, b, zeros(A.n), M_chol)
+    end
+  
+    if precond == "k-means"
+      npzwrite("data/Example20.dist$dist.nKL$nKL_trunc.P$P.p$p.amg.iters.npz", iters_amg)
+      npzwrite("data/Example20.dist$dist.nKL$nKL_trunc.P$P.p$p.chol.iters.npz", iters_chol)
+    elseif precond == "deterministic-grid"
+      npzwrite("data/Example20.m$m.P$P.p$p.amg.iters.npz", iters_amg)
+      npzwrite("data/Example20.m$m.P$P.p$p.chol.iters.npz", iters_chol)
+    end
+  end
+end
+
+
+
+
+
